ec75bfe7af7a33b1e2eb6cb3585d54cc58c6ccef
react-quiz <tim@timrich.org>
2020-09-02 15:01:38 +0300

    Updates

---
+++ precache-manifest.32dc3b518cfeb3dbd8d5381a931be83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfa3a7a18eea35c913d903b799a5451",
+    "url": "/Mantas-eng/robofriend.git/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/Mantas-eng/robofriend.git/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "25b472f516167aabdb99",
+    "url": "/Mantas-eng/robofriend.git/static/css/main.0f90ff03.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/Mantas-eng/robofriend.git/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "25b472f516167aabdb99",
+    "url": "/Mantas-eng/robofriend.git/static/js/main.11355dc7.chunk.js"
+  },
+  {
+    "revision": "7a5457d8a5a05ff87de5",
+    "url": "/Mantas-eng/robofriend.git/static/js/runtime~main.80b8ce83.js"
+  }
+]);
