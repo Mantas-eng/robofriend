16bff259ffc5cbfd7b93e418b353dea1a0944fb4
react-quiz <tim@timrich.org>
2020-09-02 15:00:50 +0300

    deploy my react app to github pages

---
+++ src/robots.js
@@ -0,0 +1,14 @@
+const robots = [
+  {id: 1,
+  name: 'Hugo',
+  username: 'Hugonator',
+  email: 'hugo@ex.ex'},
+   {id: 2,
+  name: 'Percy',
+  username: 'Accomodator',
+  email: 'come@ex.ex'},
+   {id: 3,
+  name: 'Dorm',
+  username: 'Predictor',
+  email: 'dorm@ex.ex'}  
+]
